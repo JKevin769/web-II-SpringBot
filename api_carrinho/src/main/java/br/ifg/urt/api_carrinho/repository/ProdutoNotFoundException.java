847f73cb6d92d60c01a2bef7c6fad507058a7b7b
João Kevin D. Santos <164022694+JKevin769@users.noreply.github.com>
2026-03-24 08:54:46 -0300

    Delete api_carrinho directory

--- api_carrinho/src/main/java/br/ifg/urt/api_carrinho/repository/ProdutoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.ifg.urt.api_carrinho.repository;
-
-public class ProdutoNotFoundException extends RuntimeException {
-    public ProdutoNotFoundException(String mensagem) {
-        super(mensagem);
-    }
-}
