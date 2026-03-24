bf2c8a9cf1ccd633baf9d449a1d91257e20921e0
João Kevin D. Santos <164022694+JKevin769@users.noreply.github.com>
2026-03-24 10:53:33 -0300

    Update GlobalExceptionHandler.java

--- api_carrinho/src/main/java/br/ifg/urt/api_carrinho/exception/handler/GlobalExceptionHandler.java
+++ api_carrinho/src/main/java/br/ifg/urt/api_carrinho/exception/handler/GlobalExceptionHandler.java
@@ -6,30 +6,35 @@ import br.ifg.urt.api_carrinho.exception.ProdutoNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
+//import org.springframework.web.context.request.WebRequest;
 
 import java.sql.Date;
 import java.time.LocalDateTime;
 
-@ControllerAdvice
-@RestController
+//@ControllerAdvice
+//@RestController
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     // Tratamento para Recurso Não Encontrado (404)
     @ExceptionHandler(ProdutoNotFoundException.class)
-    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(
-            Exception ex, WebRequest request) {
+//    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(
+//            Exception ex, WebRequest request) {
+//
+//        ExceptionResponse exceptionResponse = new ExceptionResponse(
+//                new Date(),
+//                ex.getMessage(),
+//                request.getDescription(false));
+//        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);}
 
-        ExceptionResponse exceptionResponse = new ExceptionResponse(
-                new Date(),
-                ex.getMessage(),
-                request.getDescription(false));
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);}
+    public ResponseEntity<ExceptionResponse> handleNotFoundExceptions(
+            ProdutoNotFoundException ex, HttpServletRequest request) {
+        return buildResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request.getRequestURI());
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionResponse> handleBadRequest(IllegalArgumentException ex, HttpServletRequest request) {
